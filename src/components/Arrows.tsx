7be63416316c16c737b4b15c079985b23f488dbf
Scott <seharlan@gmail.com>
2020-05-08 14:22:13 -0700

    styling

--- src/components/Arrows.tsx
+++ src/components/Arrows.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Arrows = () => (
   <section className="arrow-container">
-    <img className="left-arrow" src="/images/arrow.png" alt="arrow"/>
-    <img className="right-arrow" src="/images/arrow.png" alt="arrow"/>
+    <img className="left-arrow" src="/images/double-arrow.png" alt="arrow"/>
+    <img className="right-arrow" src="/images/double-arrow.png" alt="arrow"/>
   </section>
 )
 

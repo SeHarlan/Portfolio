e6298fa7b2bbd51140ad858cb116a4f4790c9b8e
Scott <seharlan@gmail.com>
2020-05-09 22:42:50 -0700

    fixed img glitch

--- src/components/EntryMessage.tsx
+++ src/components/EntryMessage.tsx
@@ -7,17 +7,17 @@ const EntryMessage = ({greeting}: {greeting: string}) => {
   const [display, setDisplay] = useState('flex')
   const [opacity, setOpacity] = useState(1)
   useEffect(() => {
-    setTimeout(() => setOpacity(0), 7700)
-    setTimeout(() => setDisplay('none'), 10000)
+    setTimeout(() => setOpacity(0), 8000)
+    setTimeout(() => setDisplay('none'), 11000)
   }, [])
   return (
     <section className="entry-container" style={{opacity: opacity, display: display}}>
       <Typist >
         <div >
-        <img src="/images/vapor-palm.png" alt="palm tree"/>
         <p>{greeting} and welcome to the personal portfolio of Scott Harlan. We hope you enjoy your visit!</p>
         </div>
       </Typist>
+      <img className="entry-image" src="/images/vapor-palm.png" alt="palm tree"/>
     </section>
   )
 };

c190ff243cf7e03e71859eb7eaf473978d4164ff
Scott <seharlan@gmail.com>
2020-05-16 17:11:18 -0700

    scrollto adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './css/App.css';
 import './css/animations.css'
 import './css/media-queries.css';
@@ -12,22 +12,21 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import EntryMessgaeProvider from './hooks/EntryMessageProvider';
 
-export default class App extends Component {
-  componentDidMount() {
+export default function App() {
+
+  useEffect(() => {
     /mobile/i.test(navigator.userAgent) && setTimeout(() => {
-      window.scrollTo(0, 5)
-    }, 1000)
-  }
-  render() {
-    return (
-      <Router>
-        <EntryMessgaeProvider>
-          <Route exact path='/' component={Home} />
-        </EntryMessgaeProvider>
-        <Route exact path='/about' component={About} />
-        <Route exact path='/projects' component={Projects} />
-        <Route exact path='/contact' component={Contact} />
-      </Router>
-    )
-  }
+          window.scrollTo(0, 5)
+        }, 1000)
+  }, [])
+  return (
+    <Router>
+      <EntryMessgaeProvider>
+        <Route exact path='/' component={Home} />
+      </EntryMessgaeProvider>
+      <Route exact path='/about' component={About} />
+      <Route exact path='/projects' component={Projects} />
+      <Route exact path='/contact' component={Contact} />
+    </Router>
+  )
 }

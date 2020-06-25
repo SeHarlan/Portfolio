03ef9dbb4d01536a59ec2fdf6cdc9bc324ea0eb8
Scott <seharlan@gmail.com>
2020-06-25 15:00:05 -0700

    fine tuned media queries

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,22 +2,24 @@ import React from 'react'
 import { useScreenDimensions } from '../hooks/useScreenDimensions'
 import jsIcon from '../assets/js-icon.png'
 
-const ProjectCard = ({right = false, title, site, github, image = jsIcon, tech, body}: {right?: boolean, title: string, site?: string, github: string, image?: string, tech: string, body: string}) => {
+const ProjectCard = ({ right = false, title, site, github, image = jsIcon, tech, body }: { right?: boolean, title: string, site?: string, github: string, image?: string, tech: string, body: string }) => {
 
-  const {width} = useScreenDimensions()
+  const { width } = useScreenDimensions()
   const className: string = (right ? "project-div-right" : "project-div")
 
-  
+  const longNameClass = title.length > 12 ? 'long-name' : ''
+
+
   // "right" boolean is used to determine which side the image is placed
   return (
-  <div className={className}>
-    {right && (<article>
+    <div className={className}>
+      {right && (<article>
         <h4>{tech}</h4>
         <p>{body}</p>
       </article>)}
-    <section>
-      <div>
-        <h3>{title}</h3>
+      <section>
+        <div>
+          <h3 className={longNameClass}>{title}</h3>
           {site && (<a href={site} target="_blank" rel="noopener noreferrer">Site</a>)}
           <a href={github} target="_blank" rel="noopener noreferrer">Github</a>
         </div>
@@ -31,6 +33,7 @@ const ProjectCard = ({right = false, title, site, github, image = jsIcon, tech, 
         <h4>{tech}</h4>
         <p>{body}</p>
       </aside>)}
-  </div>
-)}
+    </div>
+  )
+}
 export default ProjectCard

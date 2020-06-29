34ae69146047a65beac2bf05d8faf3d2bdaac6c4
Scott <seharlan@gmail.com>
2020-06-29 11:33:35 -0700

    added project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,7 @@ import HighlightedProjects from './HighlightedProjects'
 import ContactCard from './ContactCard'
 import ProjectCard from './ProjectCard'
 import waterBg from '../assets/water-bg.jpg'
+import resty from '../assets/RESTy.png'
 
 export default class Projects extends Component {
   render() {
@@ -18,16 +19,22 @@ export default class Projects extends Component {
           <header >
             <h1 className="more-projects">More Projects</h1>
           </header>
+          <ProjectCard title="REST Client"
+            image={resty}
+            github="https://github.com/SeHarlan/resty-provider"
+            tech="React / PostCSS"
+            body="REST Client (Postman clone), built in a solo two day sprint."
+          />
           <ProjectCard title="Data Structures..."
-          github="https://github.com/SeHarlan/data-structures-and-algorithms"
-          tech="Javascript"
-          body="A select series of whiteboard challenges I've completed including Linked Lists, Binary Trees, and Stacks and Queues (see branches)"
+            right={true}
+            github="https://github.com/SeHarlan/data-structures-and-algorithms"
+            tech="Javascript"
+            body="A select series of whiteboard challenges I've completed including Linked Lists, Binary Trees, and Stacks and Queues (see branches)"
           />
           <ProjectCard title="Unripe-Banana"
-          right={true}
-          github="https://github.com/SeHarlan/unripe-banana"
-          tech="Node / Express / Jest / Supertest / Mongoose / MongoDB"
-          body="A Rotten Tomato clone (back-end only) utilizing Express/Node with MongoDB aggregations and Mongoose's virtuals and static methods."
+            github="https://github.com/SeHarlan/unripe-banana"
+            tech="Node / Express / Jest / Supertest / Mongoose / MongoDB"
+            body="A Rotten Tomato clone (back-end only) utilizing Express/Node with MongoDB aggregations and Mongoose's virtuals and static methods."
           />
         </div>
 
